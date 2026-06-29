c871aa046a9bde388125f9cbe58df5599bb7605a
Rohit Pawar <rohitpawar7058@gmail.com>
2026-06-29 21:37:33 +0530

    rename the day 1 folder with the javascript fundamentals and also the practice on the data types

---
+++ Javascript Fundamentals/dataTypes.js
@@ -0,0 +1,5 @@
+let isStudent = true;
+// console.log(typeof isStudent); 
+
+let height = 5.6;
+console.log (`The type of the student is in the variable is ${isStudent} and he's height is ${height}`);
