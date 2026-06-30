383f372b9a4b745f75a4edea430707957bf9acbb
Rohit Pawar <rohitpawar7058@gmail.com>
2026-06-30 13:21:34 +0530

    Completed the data types revision

--- Javascript Fundamentals/dataTypes.js
+++ Javascript Fundamentals/dataTypes.js
@@ -1,5 +1,32 @@
-let isStudent = true;
-// console.log(typeof isStudent); 
+// // NUMBER 
+// let height = 6;
+// console.log(`The type of the variable height is ${typeof height}` );
 
-let height = 5.6;
-console.log (`The type of the student is in the variable is ${isStudent} and he's height is ${height}`);
+// // STRING
+// let name = "Rohit";
+// console.log (`THe type of the variable name is ${typeof name}`);
+
+// // BOOLEAN
+// let isStudent = true;
+// console.log ("The type of the variable isStudent is " + typeof isStudent);
+
+// // UNDEFINED 
+// let fruit ;
+// console.log (`The type of the variable fruit is ${typeof fruit}`);
+
+// //NULL
+// let user = null ;
+// console.log (`The type of the variable user is ${typeof user}`);
+
+// //BIGINT
+// let bigNumber = 123456789101112354821659923588494629494n;
+// console.log (`The type of the variable bigNumber is ${typeof bigNumber}`);
+
+// OBJECT
+const student = {
+ name: "Rohit",
+ age: 20,
+ city: 'Nashik'
+}
+console.log (student);
+
